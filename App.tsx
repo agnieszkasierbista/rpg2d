fbc311dbf69cccc939268ce41deb4bddd1d61108
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-04-06 22:48:58 +0200

    Render pictures

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import {StyleSheet, Text, View, StatusBar, Image} from 'react-native';
 
+const abc = require('./pictures/baum.png');
+const xyz = require('./pictures/water.png');
+
+const tree = {
+    bg: 'green',
+    fg: abc
+};
+const water = {
+    bg: 'blue',
+    fg: xyz
+};
 const map = [
-    [1, 2, 1, 1, 1],
-    [1, 2, 1, 2, 2],
-    [2, 2, 1, 2, 2],
-    [1, 2, 1, 1, 1],
-    [1, 2, 1, 1, 1],
+    [tree, water, tree, tree, tree],
+    [tree, water, tree, water, water],
+    [water, water, tree, water, water],
+    [tree, water, tree, tree, tree],
+    [tree, water, tree, tree, tree],
 ];
 
 // [[<View />]]
@@ -13,17 +24,22 @@ const map = [
 export default function App() {
     return (
         <View style={styles.container}>
+
             {map.map((row) => {
-                return row.map((tile) => {
+                return row.map(( tile, idx) => {
                     const bambo = {
                         width: `${100 / row.length}%`,
                         height: `${100 / map.length}%`
                     };
 
                     return (
-                        tile === 1
-                            ? <View style={[styles.tree, bambo]}/>
-                            : <View style={[styles.water, bambo]}/>
+                        <View key={idx} style={[bambo, styles.gowno]}>
+
+                            <View style={[{backgroundColor: tile.bg, flex: 1}]}/>
+                            {tile.fg ? <Image source={tile.fg} style={[ {position:"absolute",  width:'100%', height:'100%'}]} resizeMode="contain"/> : null}
+
+
+                        </View>
                     )
                 })
             })}
@@ -35,13 +51,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+    gowno: {
+        borderColor: "#523009",
+        borderStyle: "solid",
+        borderWidth: 0
+    },
     tree: {
         // flex: 1,
-        backgroundColor: 'green'
+        // backgroundColor: 'green'
     },
     water: {
         // flex: 1,
-        backgroundColor: 'blue'
+        // backgroundColor: 'blue'
     },
     container: {
         flex: 1,
