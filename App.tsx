bef2ba25bd7d1c27182a7fb7ccc6e2f15e2ae54d
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-04-06 21:30:08 +0200

    Render a basic map

--- App.tsx
+++ App.tsx
@@ -1,18 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, StatusBar} from 'react-native';
+
+const map = [
+    [1, 2, 1, 1, 1],
+    [1, 2, 1, 2, 2],
+    [2, 2, 1, 2, 2],
+    [1, 2, 1, 1, 1],
+    [1, 2, 1, 1, 1],
+];
+
+// [[<View />]]
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>asdfsaf</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            {map.map((row) => {
+                return row.map((tile) => {
+                    const bambo = {
+                        width: `${100 / row.length}%`,
+                        height: `${100 / map.length}%`
+                    };
+
+                    return (
+                        tile === 1
+                            ? <View style={[styles.tree, bambo]}/>
+                            : <View style={[styles.water, bambo]}/>
+                    )
+                })
+            })}
+            <StatusBar hidden={true}>
+
+            </StatusBar>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    tree: {
+        // flex: 1,
+        backgroundColor: 'green'
+    },
+    water: {
+        // flex: 1,
+        backgroundColor: 'blue'
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        backgroundColor: '#fff',
+    },
 });
