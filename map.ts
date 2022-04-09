fb6116e3b5b01fd18493bd2d5f21b69fef11a09b
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-04-10 00:00:06 +0200

    Don't move the map when the player is on its edges

--- map.ts
+++ map.ts
@@ -38,35 +38,57 @@ export const map = [
     [tree, water, tree, tree, tree,  tree, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
     [tree, water, tree, tree, tree, water, tree, tree, water, tree, water, tree, water, tree, water, tree, tree, water],
     [tree, water, tree, tree, tree, water, tree, tree, water, tree, water, tree, water, tree, tree, tree, tree, water],
-    [tree, water, tree, tree, tree,  tree, tree, tree, water, tree, water, tree, water, tree, tree, tree, tree, water],
-    [tree, water, tree, water, water, water, tree, tree, water, tree, water, tree, tree, tree, tree, tree, tree, water],
-    [tree, water, tree, water, water,  water, tree, tree, water, tree, water, water, tree, tree, water, tree, tree, water],
-    [water, water, tree, water, water, water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
-    [water, water, tree, water, water, water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
-    [tree, water, tree, tree, tree, water, tree, tree, water, tree, water, tree, water, tree, water, tree, tree, water],
-    [tree, water, tree, tree, tree,  water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
-    [tree, water, tree, tree, tree,  water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
-    [tree, water, tree, tree, tree, water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
+    // [tree, water, tree, tree, tree,  tree, tree, tree, water, tree, water, tree, water, tree, tree, tree, tree, water],
+    // [tree, water, tree, water, water, water, tree, tree, water, tree, water, tree, tree, tree, tree, tree, tree, water],
+    // [tree, water, tree, water, water,  water, tree, tree, water, tree, water, water, tree, tree, water, tree, tree, water],
+    // [water, water, tree, water, water, water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
+    // [water, water, tree, water, water, water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
+    // [tree, water, tree, tree, tree, water, tree, tree, water, tree, water, tree, water, tree, water, tree, tree, water],
+    // [tree, water, tree, tree, tree,  water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
+    // [tree, water, tree, tree, tree,  water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
+    // [tree, water, tree, tree, tree, water, tree, tree, water, tree, water, tree, tree, tree, water, tree, tree, water],
 ];
 
 function genRow(currentE, x) {
+    // if (x <= 3) {
+    //     return currentE
+    // }
+    const oj = x <= 3 ? 3 - x : 0 ;
+
     const tre = currentE.reduce((acc, rowTile, idx) => {
-        return idx >= x - 3 && idx <= x + 3 ?
+        return (idx >= x - 3) && (idx <= x + 3 + oj) ?
             acc.concat(rowTile) :
             acc
     }, [])
-    console.log("tre", tre)
+    // console.log("tre", tre)
     return tre
 }
 
 export function genMap(mapka, playerPos) {
     // 5x7 -->  -2 x +2 & -3 y +3
     const {x, y} = playerPos;
-    const xyz = mapka.reduce((acc, currentE, idx) => {
-        return idx >= y - 3 && idx <= y + 3 ?
+
+    const oj = y <= 3 ? 3 - y : 0 ;
+    const mapHeight = mapka.length - 1;
+
+    console.log('mapHeight', mapHeight);
+    console.log('y',y);
+    console.log('oj', oj)
+
+    const czoko = y >= mapHeight - 3 ? (mapHeight - y) : 0;
+
+    console.log('czoko', czoko);
+
+    const abc = (mapHeight - y <= 3) ? 3 - (mapHeight - y) : 0 ;
+
+    console.log('abc', abc);
+
+    const xyz = mapka.reduce((acc, currentE, mapY) => {
+
+        return (mapY >= y - 3 - abc) && (mapY <= y + 3 + oj) ?
             acc.concat([genRow(currentE, x)]) :
             acc
     }, [])
-    console.log(xyz);
+    // console.log(xyz);
     return xyz
 }
